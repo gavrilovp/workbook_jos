d87ea7cb172f0135aaaf0f6c5cc4fa3ad6f054b0
Gavrilov Pavel <pavelgavrilov19@gmail.com>
2015-12-19 01:22:54 +0300

    implements user_mem_check

--- labs/kern/init.c
+++ labs/kern/init.c
@@ -43,8 +43,10 @@ i386_init(void)
 	ENV_CREATE2(TEST, TESTSIZE);
 #else
 	// Touch all you want.
-	ENV_CREATE(user_hello);
+	/* ENV_CREATE(user_hello); */
 	/* ENV_CREATE(user_divzero); */
+	ENV_CREATE(user_buggyhello);
+	/* ENV_CREATE(user_evilhello); */
 #endif // TEST*
 
 
