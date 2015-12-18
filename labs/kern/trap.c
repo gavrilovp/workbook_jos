2e9d7682d15f741ec5d1b1087bb0137ca2c7f25a
Gavrilov Pavel <pavelgavrilov19@gmail.com>
2015-12-19 01:03:20 +0300

    handles kernel page faults

--- labs/kern/trap.c
+++ labs/kern/trap.c
@@ -189,9 +189,9 @@ page_fault_handler(struct Trapframe *tf)
 	fault_va = rcr2();
 
 	// Handle kernel-mode page faults.
+	if ((tf->tf_cs & 3) == 0)
+		panic("Kernel page fault");
 	
-	// LAB 3: Your code here.
-
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
 
